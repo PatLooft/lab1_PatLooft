eed8c86ee9dc04cac529d14de97b2c4c3edf5944
PatL <patl@temple.edu>
2019-02-20 13:05:04 -0500

    Initial Commit

---
+++ lab1_PatLooft/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  lab1_PatLooft
+//
+//  Created by Pat Looft on 2/20/19.
+//  Copyright © 2019 Pat Looft. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
